39b55a4598e34d4c7e4de14fc38cb39944f7a10d
GTrunSec <gtrunsec@hardenedlinux.org>
2022-09-28 22:18:23 -0700

    add update shell

--- nix/julia2nix/devshells/default.nix
+++ nix/julia2nix/devshells/default.nix
@@ -64,6 +64,7 @@ in
             --refresh --command \
             nvfetcher-update nix/julia2nix/packages/toolchain/sources.toml
           '';
+          help = "Generate nix sources expr for the latest version of packages";
         }
       ];
 
@@ -77,6 +78,20 @@ in
     packages = {pkgs, ...}: {
       imports = [
         cell.devshellProfiles.packages
+      ];
+    };
+
+    update = {...}: {
+      commands = [
+        {
+          name = "nvfetcher-update";
+          command = ''
+            nix develop github:GTrunSec/cells-lab#devShells.x86_64-linux.update \
+            --refresh --command \
+            nvfetcher-update nix/julia2nix/packages/toolchain/sources.toml
+          '';
+          help = "Generate nix sources expr for the latest version of packages";
+        }
       ];
     };
   }
