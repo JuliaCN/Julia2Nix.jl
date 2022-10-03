256f77581ccb20b2fc54544f95a8846bdf5d52b0
GTrunSec <gtrunsec@hardenedlinux.org>
2022-10-02 20:43:32 -0700

    add update.shell

--- nix/julia2nix/devshells/default.nix
+++ nix/julia2nix/devshells/default.nix
@@ -28,6 +28,7 @@ in
           cell.devshellProfiles.packages
           # cell.devshellProfiles.nightly
           cell.devshellProfiles.checks
+          cell.devshellProfiles.update
         ]
         ++ l.optionals nixpkgs.stdenv.buildPlatform.isLinux [
           cell.devshellProfiles.dev
@@ -55,15 +56,6 @@ in
           command = "nix run .#packages.${nixpkgs.system}.build-project --print-build-logs -- testenv/writejulia2nix.jl ${nixpkgs.system}";
           help = "write julia2nix.toml with buildProject";
         }
-        {
-          name = "nvfetcher-update";
-          command = ''
-            nix develop github:GTrunSec/cells-lab#devShells.x86_64-linux.update \
-            --refresh --command \
-            nvfetcher-update nix/julia2nix/packages/toolchain/sources.toml
-          '';
-          help = "Generate nix sources expr for the latest version of packages";
-        }
       ];
 
       env = [
@@ -80,16 +72,8 @@ in
     };
 
     update = {...}: {
-      commands = [
-        {
-          name = "nvfetcher-update";
-          command = ''
-            nix develop github:GTrunSec/cells-lab#devShells.x86_64-linux.update \
-            --refresh --command \
-            nvfetcher-update nix/julia2nix/packages/toolchain/sources.toml
-          '';
-          help = "Generate nix sources expr for the latest version of packages";
-        }
+      imports = [
+        cell.devshellProfiles.update
       ];
     };
   }
