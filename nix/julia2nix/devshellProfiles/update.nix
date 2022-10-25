c05f9b78525607956a033e23ac737b7035dde3bc
GTrunSec <gtrunsec@hardenedlinux.org>
2022-10-25 00:06:08 -0700

    fix nvfetcher nightly

--- nix/julia2nix/devshellProfiles/update.nix
+++ nix/julia2nix/devshellProfiles/update.nix
@@ -14,6 +14,9 @@
         nix develop github:GTrunSec/cells-lab#devShells.x86_64-linux.update \
         --refresh --command \
         nvfetcher-update nix/julia2nix/packages/toolchain/sources.toml
+        if [[ ! -n "$(git diff HEAD --unified=0 | grep 'sha256')" && -n "''${GITHUB_ENV}" ]]; then
+           unset GITHUB_ENV
+        fi
       '';
       help = "Generate nix sources expr for the latest version of packages";
     }
