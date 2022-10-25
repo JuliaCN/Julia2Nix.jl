4be0d1a7c5b9c1b7dd67a75a7c48d6ee7102162a
GTrunSec <gtrunsec@hardenedlinux.org>
2022-10-25 00:54:51 -0700

    CI: fix autoupdate

--- nix/julia2nix/devshellProfiles/update.nix
+++ nix/julia2nix/devshellProfiles/update.nix
@@ -13,10 +13,7 @@
       command = ''
         nix develop github:GTrunSec/cells-lab#devShells.x86_64-linux.update \
         --refresh --command \
-        nvfetcher-update nix/julia2nix/packages/toolchain/sources.toml
-        if [[ ! -n "$(git diff HEAD --unified=0 | grep 'sha256')" && -n "''${GITHUB_ENV}" ]]; then
-           unset GITHUB_ENV
-        fi
+        nvfetcher-update-force nix/julia2nix/packages/toolchain/sources.toml
       '';
       help = "Generate nix sources expr for the latest version of packages";
     }
